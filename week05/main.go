58be2d0b356ff054799c72ece33929f1a14cbb6d
ruslanbekovaaidana8 <ruslanbekovaaidana8@mail.com>
2024-09-27 15:15:25 +0900

    v0.2

---
+++ week05/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"strings"
+)
+
+func main() {
+	var f float32 = 3.99
+	i := 55
+	fmt.Println(f, math.Ceil(3.49))
+	fmt.Println(strings.Title("KIM inha"))
+	fmt.Println("i는 %d\n", i)
+	fmt.Println("i는", i, "\n")
+	fmt.Println("i는", i)
+}
