2270a627df4b7415f243b0a3769d145515ed8142
ruslanbekovaaidana8 <ruslanbekovaaidana8@mail.com>
2024-10-04 15:49:00 +0900

    v0.7 Zero value

--- week06/main.go
+++ week06/main.go
@@ -1,21 +1,32 @@
 package main
 
 import (
-	"fmt"     // Подключаем пакет для форматированного вывода
-	"reflect" // Подключаем пакет для работы с типами данных
+	"fmt" // Подключаем пакет для форматированного вывода
+	//"reflect" // Подключаем пакет для работы с типами данных
 )
 
 func main() {
-	i := 13
-	var f float64 = 12.9 // Объявляем переменную с плавающей запятой 'f' и присваиваем значение 12.9
-	c1 := 'z'
-	c2 := "kim" //44688
+	var f float64
+	var i int
+	var b bool
+	var s string
+	//mySchoolAccount := 5
+	//println(mySchoolAccount)
+	fmt.Println(f, b, s, i)
+	fmt.Printf("%f%t%s%d\n", f, b, s, i)
+	f = 2.7
+	i = 3
+	fmt.Print("\n\n", (f <= float64(i)), "\n")
+	//i := 13
+	//var f float64 = 12.9 // Объявляем переменную с плавающей запятой 'f' и присваиваем значение 12.9
+	//c1 := 'z'
+	//c2 := "kim" //44688
 	// Корректный форматированный вывод с помощью fmt.Printf
-	fmt.Printf("value i: %d, value f: %f\n", i, f)
+	//fmt.Printf("value i: %d, value f: %f\n", i, f)
 
 	// Приведение типа 'f' к int для умножения и корректный вывод
-	fmt.Printf("%d * %f = %d\n", i, f, 1*int(f))
-	fmt.Println(c1, c2)
+	//fmt.Printf("%d * %f = %d\n", i, f, 1*int(f))
+	//fmt.Println(c1, c2)
 	// Вывод типа переменных
-	fmt.Println(reflect.TypeOf(i), reflect.TypeOf(f), reflect.TypeOf(c1), reflect.TypeOf(c2)) // Выводим тип переменных 'i' и 'f'
+	//fmt.Println(reflect.TypeOf(i), reflect.TypeOf(f), reflect.TypeOf(c1), reflect.TypeOf(c2)) // Выводим тип переменных 'i' и 'f'
 }
