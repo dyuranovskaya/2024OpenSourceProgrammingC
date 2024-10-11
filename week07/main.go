a49b596fc5eaed8ad5b3f84bb1bf50cc8e9718d1
ruslanbekovaaidana8 <ruslanbekovaaidana8@mail.com>
2024-10-11 14:28:16 +0900

    v1.1 Stdin

--- week07/main.go
+++ week07/main.go
@@ -1,12 +1,20 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"time"
+	"os"
 )
 
 func main() {
-	var now time.Time = time.Now()
-	fmt.Printf("today %dyear %dmonth %dday.\n", now.Year(), now.Month(), now.Day())
-	fmt.Printf("now %dhour %dminute %dsecond.\n", now.Hour(), now.Minute(), now.Second())
+	//var now time.Time = time.Now()
+	//fmt.Printf("today %dyear %dmonth %dday.\n", now.Year(), now.Month(), now.Day())
+	//fmt.Printf("now %dhour %dminute %dsecond.\n", now.Hour(), now.Minute(), now.Second())
+	//var army string = "오늘은 $가와 $만에 중서을 다하는 대한민 $육군임니다"
+	//armyField := strings.NewReplacer("$", "국")
+	//fmt.Println(army)
+	//fmt.Println(armyField.Replace(army))
+	in := bufio.NewReader(os.Stdin)
+	i, err := in.ReadString("\n")
+	fmt.Println(i, err)
 }
