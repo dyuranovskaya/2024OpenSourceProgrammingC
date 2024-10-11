cbc2d866872d01a4fe452f8263e774bc2557837f
ruslanbekovaaidana8 <ruslanbekovaaidana8@mail.com>
2024-10-11 14:57:33 +0900

    v1.2 If

--- week07/main.go
+++ week07/main.go
@@ -3,18 +3,18 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
 )
 
 func main() {
-	//var now time.Time = time.Now()
-	//fmt.Printf("today %dyear %dmonth %dday.\n", now.Year(), now.Month(), now.Day())
-	//fmt.Printf("now %dhour %dminute %dsecond.\n", now.Hour(), now.Minute(), now.Second())
-	//var army string = "오늘은 $가와 $만에 중서을 다하는 대한민 $육군임니다"
-	//armyField := strings.NewReplacer("$", "국")
-	//fmt.Println(army)
-	//fmt.Println(armyField.Replace(army))
 	in := bufio.NewReader(os.Stdin)
-	i, err := in.ReadString("\n")
-	fmt.Println(i, err)
+	fmt.Print("Input your name : ")
+	name, err := in.ReadString('\n')
+
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		fmt.Println(name)
+	}
 }
