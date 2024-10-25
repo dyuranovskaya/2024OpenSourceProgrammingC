f90f4a9cac6231c7fd245351d549b3570fa6a768
ruslanbekovaaidana8 <ruslanbekovaaidana8@mail.com>
2024-10-25 14:34:10 +0900

    Guess game v1.2

--- week09/main.go
+++ week09/main.go
@@ -1,3 +1,29 @@
+// func main() {
+// 	answer := rand.Intn(6) + 1
+// 	fmt.Println(answer)
+
+// fmt.Print("number 입력 : ")
+// in := bufio.NewReader(os.Stdin)
+// i, err := in.ReadString('\n')
+// if err != nil {
+// 	log.Fatal(err)
+// 	}
+
+// 	i = strings.TrimSpace(i)
+// 	quess, err := strconv.Atoi(i)
+
+//		if err != nil {
+//			log.Fatal(err)
+//		}
+//		fmt.Println(quess)
+//		if answer == quess {
+//			fmt.Println("정답입니다!")
+//		} else if answer > quess {
+//			fmt.Println("입력하신 수는 정보다 작은 수 입니다. LOW")
+//		} else {
+//			fmt.Println("입력하신 수는 정보다 작은 수 입니다.HIGH")
+//		}
+//	}
 package main
 
 import (
@@ -8,31 +34,35 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"time"
 )
 
 func main() {
+	rand.Seed(time.Now().Unix())
 	answer := rand.Intn(6) + 1
 	fmt.Println(answer)
+	for guesses := 0; guesses < 3; guesses++ {
+		fmt.Println("점수 임력:")
+		in := bufio.NewReader(os.Stdin)
+		i, err := in.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+		i = strings.TrimSpace(i)
 
-	fmt.Print("number 입력 : ")
-	in := bufio.NewReader(os.Stdin)
-	i, err := in.ReadString('\n')
-	if err != nil {
-		log.Fatal(err)
+		guess, err := strconv.Atoi(i)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(guess)
+
+		if answer == guess {
+			fmt.Println("정답입니다!")
+		} else if answer > guess {
+			fmt.Println("입력하신 수는 정보다 작은 수 입니다. LOW")
+		} else {
+			fmt.Println("입력하신 수는 정보다 작은 수 입니다. HIGH")
+		}
 	}
 
-	i = strings.TrimSpace(i)
-	quess, err := strconv.Atoi(i)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(quess)
-	if answer == quess {
-		fmt.Println("정답입니다!")
-	} else if answer > quess {
-		fmt.Println("입력하신 수는 정보다 작은 수 입니다. LOW")
-	} else {
-		fmt.Println("입력하신 수는 정보다 작은 수 입니다.HIGH")
-	}
 }
