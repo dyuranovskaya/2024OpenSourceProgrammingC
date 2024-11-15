4182bd809895000e5d27d0ffd6215863455340ab
ruslanbekovaaidana8 <ruslanbekovaaidana8@mail.com>
2024-11-15 15:22:45 +0900

    v3.9Array

--- week12/main.go
+++ week12/main.go
@@ -2,32 +2,19 @@ package main
 
 import (
 	"fmt"
-	"time"
+
+	"github.com/headfirstgo/keyboard"
 )
 
 func main() {
-	var scores [3]int
-	scores[1] = 90
-	// fmt.Println(scores[1], scores[0])
-	// fmt.Printf("%#v\n", scores)
-	// fmt.Println(scores)
+	var gpas [3]float64
+	for i := 0; i < len(gpas); i++ {
+		fmt.Print("input float number:")
+		gpas[i], _ = keyboard.GetFloat()
 
-	// for i := 0; i <= len(scores); i++ {
-	for i := 0; i < len(scores); i++ {
-		fmt.Printf("%d", scores[i])
 	}
-	// dates:= [3]time.Time=[3]time.Time{time.Unix(0, 0),time.Unix(1947200001, 0),time.Unix(0, 0)}
-	// fmt.Println(dates[0],dates[1],dates[2])
-	dates := [3]time.Time{
-		time.Unix(0, 0),
-		time.Unix(1, 0),
-		time.Unix(1947200001, 0)}
-	// fmt.Println(dates[0], dates[1], dates[2])
-	// fmt.Printf("%#v\n", dates)
-	// fmt.Println(dates)
-	fmt.Println()
-	for _, date := range dates {
-		fmt.Println(date)
+	for _, gpa := range gpas {
+		fmt.Println(gpa)
 	}
 
 }
