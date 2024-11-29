43ab9f0e4acb3eb7c21347fc989ba318062ea0c1
ruslanbekovaaidana8 <ruslanbekovaaidana8@mail.com>
2024-11-29 14:51:21 +0900

    v4.8 TypeStruct

--- week14/main.go
+++ week14/main.go
@@ -2,22 +2,20 @@ package main
 
 import "fmt"
 
+type student struct {
+	id   int
+	name string
+	gpa  float32
+}
+
 func main() {
-	var student1 struct {
-		id   int
-		name string
-		gpa  float32
-	}
+	var student1 student
 	student1.id = 202444901
 	student1.name = "Aidana"
 	student1.gpa = 4.0
 	fmt.Println(student1.gpa)
 
-	var student2 struct {
-		id   int
-		name string
-		gpa  float32
-	}
+	var student2 student
 	student2.id = 2024556523
 	student2.name = "Kimchol"
 	student2.gpa = 4.45
